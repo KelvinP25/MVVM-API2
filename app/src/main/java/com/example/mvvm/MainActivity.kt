871f4299fccd35033b344c0021f6690385569f9a
KelvinP25 <kelvincastillo9911@gmail.com>
2022-04-29 21:13:23 -0400

    Creando el dinding

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvm.databinding.ActivityMainBinding
 
-private lateinit var biding : AppCompatActivity
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
