e07f28a82ffedb67f5f52e71c3d5d39b6b1c3dc2
KelvinP25 <kelvincastillo9911@gmail.com>
2022-04-29 20:23:00 -0400

    Agregando dependencias para MVVM

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+private lateinit var biding : AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
