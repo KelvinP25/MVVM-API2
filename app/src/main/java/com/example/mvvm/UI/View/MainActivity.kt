532c7378a4e7f20c9e1c61d10d01f68594a8bf65
KelvinP25 <kelvincastillo9911@gmail.com>
2022-05-02 23:21:38 -0400

    Arreglando el primer cargado de la app

--- app/src/main/java/com/example/mvvm/UI/View/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/UI/View/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mvvm.UI.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import com.example.mvvm.databinding.ActivityMainBinding
 import com.example.mvvm.UI.viewmodel.QuotteViewModel
@@ -18,9 +19,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        QuotteViewModel.onCreate();
+
         QuotteViewModel.quoteModel.observe(this, Observer {currentQuote ->
             binding.tvQuote.text = currentQuote.quote
             binding.tvAuthor.text = currentQuote.author
+        })
+
+        QuotteViewModel.isLoading.observe(this, Observer {
+            binding.progress.isVisible = it
         })
         binding.viewContainer.setOnClickListener{ QuotteViewModel.randomQuote()}
     }
--- app/src/main/java/com/example/mvvm/UI/viewmodel/QuotteViewModel.kt
+++ app/src/main/java/com/example/mvvm/UI/viewmodel/QuotteViewModel.kt
@@ -2,15 +2,32 @@ package com.example.mvvm.UI.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mvvm.data.model.model.QuoteModel
 import com.example.mvvm.data.model.model.QuoteProvider
+import com.example.mvvm.domain.GetQuotesUseCase
+import kotlinx.coroutines.launch
 
 
 class QuotteViewModel : ViewModel(){
     val quoteModel = MutableLiveData<QuoteModel>()
+    val isLoading = MutableLiveData<Boolean>()
 
-    fun randomQuote(){
-        val currentQuote = QuoteProvider.random()
-        quoteModel.postValue(currentQuote)
+    var getQuotesUseCase = GetQuotesUseCase()
+    fun onCreate() {
+        viewModelScope.launch {
+            isLoading.postValue(true)
+            val result = getQuotesUseCase()
+            if (!result.isNullOrEmpty()){
+                quoteModel.postValue(result[0])
+                isLoading.postValue(false)
+            }
+        }
     }
+    fun randomQuote(){
+        //val currentQuote = QuoteProvider.random()
+        //quoteModel.postValue(currentQuote)
+    }
+
+
 }
